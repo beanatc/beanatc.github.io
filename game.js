aa8da2e0b006d71ea225ea6158eec4cb4402bc96
BookSalmon <123578256+HaxeFloppa@users.noreply.github.com>
2025-02-24 16:23:40 +0000

    working on the actual plane requests

--- game.js
+++ game.js
@@ -1,12 +1,13 @@
-const planeTypes = [""]
+const planeTypes = ["AIR CANADA", "LUFTHANSA", "DELTA", "AIRFRANS", "RYR", "EK"];
 
 function sendPlaneRequest() {
     const reqSuffix = "req";
     let chosenReq = Math.floor(Math.random() * 6);
     let planeNum = Math.floor(Math.random() * 1001);
     let randomReqID = reqSuffix.concat("", chosenReq.toString());
-    let reqFrom = "Plane".concat("-", planeNum.toString());
+    let reqFrom = planeTypes[Math.floor(Math.random()*7)].concat("-", planeNum.toString());
     const reqButton = document.getElementById(randomReqID).innerHTML=reqFrom;
+    const reqButtonBG = document.getElementById(randomReqID).backgroundColor="yellow";
 }
 
 function PlaneRequest(incomingReqID) {
@@ -14,8 +15,12 @@ function PlaneRequest(incomingReqID) {
     const reqButton = document.getElementById(reqID);
     if (reqButton.innerHTML == "No request.") {
         alert("There is no request here.");
+    } else if (reqButton.innerHTML == "Opened.") {
+        alert("You already have this opened.");
     } else {
-
+        const reqButton = document.getElementById(randomReqID).innerHTML="Opened.";
+        const reqButtonBG = document.getElementById(randomReqID).backgroundColor="black";
+        const reqButtonTC = document.getElementById(randomReqID).color="white";
     };
 }
 
