2a2b020e65870c19faaf61fd85b37bbd7fe98584
BookSalmon <123578256+HaxeFloppa@users.noreply.github.com>
2025-02-24 16:39:02 +0000

    forgot to change a parameter

--- game.js
+++ game.js
@@ -3,7 +3,7 @@ const planeTypes = ["AIR CANADA", "LUFTHANSA", "DELTA", "AIRFRANS", "RYR", "EK"]
 function sendPlaneRequest() {
     const reqSuffix = "req";
     let chosenReq = Math.floor(Math.random() * 6);
-    let planeNum = Math.floor(Math.random() * 1001);
+    let planeNum = Math.floor(Math.random() * 10000);
     let randomReqID = reqSuffix.concat("", chosenReq.toString());
     let reqFrom = planeTypes[Math.floor(Math.random()*7)].concat("-", planeNum.toString());
     const reqButton = document.getElementById(randomReqID).innerHTML=reqFrom;
@@ -18,9 +18,9 @@ function PlaneRequest(incomingReqID) {
     } else if (reqButton.innerHTML == "Opened.") {
         alert("You already have this opened.");
     } else {
-        const reqButton = document.getElementById(randomReqID).innerHTML="Opened.";
-        const reqButtonBG = document.getElementById(randomReqID).style.backgroundColor="black";
-        const reqButtonTC = document.getElementById(randomReqID).style.color="white";
+        const reqButton = document.getElementById(reqID).innerHTML="Opened.";
+        const reqButtonBG = document.getElementById(reqID).style.backgroundColor="black";
+        const reqButtonTC = document.getElementById(reqID).style.color="white";
     };
 }
 
