b79ce196e6e61fda1661cd90a860ca24a5a2cbcd
BookSalmon <123578256+HaxeFloppa@users.noreply.github.com>
2025-02-24 11:25:49 +0000

    setting up plane request system

--- game.js
+++ game.js
@@ -1,5 +1,18 @@
+function sendPlaneRequest() {
+    const reqSuffix = "req";
+    let chosenReq = Math.random() * 6
+    let planeNum = Math.random() * 1001
+    let randomReqID = reqSuffix.concat(" ", chosenReq.toString());
+    let reqFrom = "Plane".concat(" ", planeNum.toString());
+    const reqButton = document.getElementById(randomReqID).innerHTML=reqFrom;
+}
+
 function Game() {
-    alert("game");
+    alert("Good luck!");
     const startButton = document.getElementById("startGame");
     startButton.remove();
+    while (true) {
+        setTimeout(Math.floor(Math.random() * 200001))
+        sendPlaneRequest();
+    }
 }
